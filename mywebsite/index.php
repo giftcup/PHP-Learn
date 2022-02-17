ea7c73f4eb7e8060b50df5c6a4997cc074a28f39
giftcup <salometambe@gmail.com>
2022-02-17 23:31:39 +0100

    Introduction to php

---
+++ mywebsite/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <p>This is a paragraph</p>
+    <?php
+        // This is a comment
+        /*
+            This is also a comment
+        */
+        echo "Hello World";
+    ?> 
+    <p>This is a paragraph</p>
+
+</body>
+</html>
