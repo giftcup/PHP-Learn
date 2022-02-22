aa3a9b54af11744a8cfa049d5f85dc90e6cbf58d
giftcup <salometambe@gmail.com>
2022-02-22 16:29:27 +0100

    Writing to the database using prepared statements

--- loginsystem/includes/signup.php
+++ loginsystem/includes/signup.php
@@ -9,7 +9,16 @@
     $pwd = mysqli_real_escape_string($conn, $_POST['uid']);
 
     $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd)
-    VALUES ('$first', '$last', '$email', '$uid', '$pwd');";
-    mysqli_query($conn, $sql);
+    VALUES (?, ?, ?, ?, ?);";
+
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo "SQL error";
+    }
+    else {
+        echo "Later";
+        mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $uid, $pwd);
+        mysqli_stmt_execute($stmt);
+    }
 
     header("Location: ../index.php?signup=success");
