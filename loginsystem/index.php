c77544959fe557cc48c2df573a779576896d0e16
giftcup <salometambe@gmail.com>
2022-02-22 13:51:32 +0100

    A simple login form that collects user input and inhibits sql code execution

---
+++ loginsystem/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+
+<form action="includes/signup.php" method="POST">
+    <input type="text" name="first" placeholder="Firstname">
+    <br>
+    <input type="text" name="last" placeholder="Lastname">
+    <br>
+    <input type="text" name="email" placeholder="E-mail">
+    <br>
+    <input type="text" name="uid" placeholder="Username">
+    <br>
+    <input type="password" name="pwd" placeholder="Password">
+    <br>
+    <button type="submit" name="submit">Signup</button>
+</form>
+    
+</body>
+</html>
