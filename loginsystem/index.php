64dddabdd4e0a8a6bf76a6fa5e9dc737cc2725f5
giftcup <salometambe@gmail.com>
2022-02-22 16:28:35 +0100

    Reading from the database using prepared statements

--- loginsystem/index.php
+++ loginsystem/index.php
@@ -11,18 +11,27 @@
 </head>
 <body>
 
-<?php
+ <?php
+    $data = "Admin";
+    $sql = "SELECT * FROM users WHERE user_uid=?;";
 
-        $sql = "SELECT * FROM users WHERE user_last='Doe';";
-        $result = mysqli_query($conn, $sql);
-
-        $resultCheck = mysqli_num_rows($result);
-
-        if ($resultCheck > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo $row['user_first']. "<br>";
-            }
+    // Create a prepared statement
+    $stmt = mysqli_stmt_init($conn);
+    //Prepare the prepared statement
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo "SQL statement failed";
+    }
+    else {
+        // Bind parameters to the placeholders
+        mysqli_stmt_bind_param($stmt, "s", $data);
+        // Run parameters inside database
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo $row['user_first']. "<br>";
         }
+    }
+    mysqli_close($conn);
 ?>
 
 <form action="includes/signup.php" method="POST">
