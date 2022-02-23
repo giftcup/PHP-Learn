b3d3d61568d6e55afd0b693b9000eba99ff5e023
giftcup <salometambe@gmail.com>
2022-02-23 17:53:35 +0100

    include files linking the database to the application

---
+++ loginsystem/includes/signup.inc.php
@@ -0,0 +1,45 @@
+<?php
+
+    if (!isset($_GET['signup-submit'])) {
+        header("Location: ../signup.php?signup=error");
+        exit();
+    }
+   else {
+    require "./database.inc.php";
+
+    $uid = mysqli_real_escape_string($conn, $_POST['uid']);
+    $mail = mysqli_real_escape_string($conn, $_POST['mail']);
+    $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+    $pwdRepeat = mysqli_real_escape_string($conn, $_POST['pwd-repeat']);
+
+    if (empty($uid) || empty($mail) || empty($pwd) || empty($pwdRepeat)) {
+        header("Location: ../signup.php?signup=empty&uid=$uid&mail=$mail");
+        exit();
+    }
+
+    if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        header("Location: ../signup.php?signup=email&uid=$uid&mail=$mail");
+        exit();
+    }
+
+    if ($pwd !== $pwdRepeat) {
+        header("Location: ../signup.php?signup=nomatch&uid=$uid&mail=$mail");
+        exit();
+    }
+    else {
+        $pwdHashed = password_hash($pwd, PASSWORD_DEFAULT);
+    }
+
+    $stmt = mysqli_stmt_init($conn);
+    $sql = "INSERT INTO users (uname, uemail, upwd) VALUES (?,?,?);";
+
+    if(!mysqli_stmt_prepare($stmt, $sql)) {
+        echo 'SQL Error';
+        header("Location: ../index.php?signup=sqlfailure");
+    }
+    else {
+        mysqli_stmt_bind_param($stmt, "sss", $uid, $mail, $pwdHashed);
+        mysqli_stmt_execute($stmt);
+        header("Location: ../login.php?signup=success");
+    }
+}
