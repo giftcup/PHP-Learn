cf4e175e588706497fd296a3514b9e4b5924a5fa
giftcup <salometambe@gmail.com>
2022-02-24 09:02:30 +0100

    Added error messages on front page

--- loginsystem/signup.php
+++ loginsystem/signup.php
@@ -7,13 +7,20 @@
             <section class="input-page">
                 <h1 class="input-page-head">Signup</h1>
                 <form action="includes/signup.inc.php" method="post" class="input-form">
-                    <?php 
+                    <?php
+                        if (isset($_GET['signup'])) {
+                            $signupVal = $_GET['signup'];
+                        }
+
                         if(!isset($_GET['uid'])) {
                             echo  '<input type="text" name="uid" placeholder="Username">';
                         }
                         else {
                             $uid = $_GET['uid'];
                             echo '<input type="text" name="uid" value="'.$uid.'">';
+                            if ($signupVal == "userNameTaken") {
+                                echo '<p class="perror">User name unavailable</p>';
+                            }
                         }
                         if(!isset($_GET['mail'])) {
                             echo  '<input type="text" name="mail" placeholder="E-mail">';
@@ -21,6 +28,9 @@
                         else {
                             $mail = $_GET['mail'];
                             echo '<input type="text" name="mail" value="'.$mail.'">';
+                            if ($signupVal == "emailRegistered") {
+                                echo '<p class="perror">There is an account with this email already</p>';
+                            }
                         }
                     ?>
                     <input type="password" name="pwd" placeholder="Password">
@@ -42,9 +52,11 @@
                             }
                             else if ($signupVal == "email") {
                                 echo '<p>Incorrect format for email!</p>';
+                                exit();
                             }
                             else if ($signupVal == "error") {
                                 echo '<p>Submit the form!!!</p>';
+                                exit();
                             }
                         }
                     ?>
