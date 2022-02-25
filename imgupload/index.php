390ddf239c0b9bf7407f0f8f4d21fe360cc54dc1
giftcup <salometambe@gmail.com>
2022-02-25 23:37:51 +0100

    Upload imageswhen user is logged in

---
+++ imgupload/index.php
@@ -0,0 +1,32 @@
+<?php 
+    session_start();
+    include_once 'dbh.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    
+
+    <form action="upload.php">
+        <input type="file" name="file">
+        <button type="submit" name="file-submit">UPLOAD</button>
+    </form>
+
+    <p>Login as User</p>
+    <form action="login.php" method="POST">
+        <button type="submit" name="submitLogin">Login</button>
+    </form>
+
+    <p>Login out User</p>
+    <form action="logout.php" method="POST">
+        <button type="submit" name="submitLogout">Logout</button>
+    </form>
+
+</body>
+</html>
