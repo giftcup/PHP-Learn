e633e62b00860f8a3eab0b56c891e58c3e04a209
giftcup <salometambe@gmail.com>
2022-03-11 18:33:55 +0100

    Add profile images

---
+++ loginsystem/includes/upload.inc.php
@@ -0,0 +1,69 @@
+<?php
+
+session_start();
+include 'database.inc.php';
+
+if (!isset($_POST['submit-upload'])) {
+    header("Location: ../index.php?upload=failure"); 
+}
+
+$file = $_FILES['profile'];
+// print_r($file);
+
+$fileName = $file['name'];
+$fileTmpName = $file['tmp_name'];
+$fileError = $file['error'];
+$fileSize = $file['size'];
+
+$fileExt = explode('.', $fileName);
+$fileActExt = strtolower(end($fileExt));
+
+$allow = array('jpg', 'jpeg', 'png');
+
+if ($fileError == 1) {
+    header("Location: ../index.php?upload=error");
+    exit();
+}
+if (!in_array($fileActExt, $allow)) {
+    header("Location: ../index.php?upload=not_allowed");
+    exit();
+}
+if ($fileSize > 1000000) {
+    header ("Location: ../index.php?uploadfailure=large");
+    exit();
+}
+
+$id = $_SESSION['userId'];
+
+$fileNewName = 'profile'.$id.'.'.$fileActExt;
+$destination = '../uploads/'.$fileNewName;
+move_uploaded_file($fileTmpName, $destination);
+
+$sql = "UPDATE profileimg SET status=1 WHERE user_id=?;";
+
+$stmt = mysqli_stmt_init($conn);
+if (!mysqli_stmt_prepare($stmt, $sql)) {
+    header ("Location: ../index.php?upload=error");
+}
+mysqli_stmt_bind_param($stmt, "i", $id);
+mysqli_stmt_execute($stmt);
+
+
+$sql = "SELECT * FROM profileimg WHERE user_id=?;";
+$stmt = mysqli_stmt_init($conn);
+if (!mysqli_stmt_prepare($stmt, $sql)) {
+    header ("Location: ../index.php?upload=error");
+    exit();
+}
+mysqli_stmt_bind_param($stmt, "i", $id);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+if ($result > 1 || $result < 0) {
+    header("Location: ../index.php?upload=error");
+    exit();
+}
+if ($row = mysqli_fetch_assoc($result)) {
+    $_SESSION['userImage'] = $row['status'];
+}
+
+header("Location: ../index.php?upload=success");
