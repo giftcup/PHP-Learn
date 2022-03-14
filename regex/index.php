7e50a067d68d7f0bb9eb78f2c0c1cb62457cbc56
giftcup <salometambe@gmail.com>
2022-03-14 22:52:04 +0100

    Using basic regex functions

--- regex/index.php
+++ regex/index.php
@@ -7,11 +7,22 @@
 </head>
 <body>
     <?php 
-        $string = "My name is Salome, Salome is my name";
-        $pattern = "/salome/i";
+        $string = "My name is Salome, salome is my name\nI mean a lot to me";
+        $pattern = "/(sal)(ome)/i";
+        $pattern2 = "/me/m";
+        $pattern3 = "/is/";
+        $replacement = "will be";
+        
         if (preg_match($pattern, $string, $array)) {
             print_r($array);
         }
+        
+        if (preg_match_all($pattern2, $string, $array)) {
+            print_r($array);
+        }
+
+        $newString = preg_replace($pattern3, $replacement, $string);
+        echo $newString;
     ?>
 </body>
 </html>
