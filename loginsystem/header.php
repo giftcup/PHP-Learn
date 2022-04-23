d472aef1b3aba8902dea503aea347e05cdc5d665
giftcup <salometambe@gmail.com>
2022-04-23 23:25:08 +0100

    Display profile image of the user being messaged and shows the messages only upon login

--- loginsystem/header.php
+++ loginsystem/header.php
@@ -1,9 +1,10 @@
-<?php 
-    session_start();
+<?php
+session_start();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,19 +12,24 @@
     <link rel="stylesheet" href="./style.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <body>
-    
+
     <header class="header">
-        <nav class="nav-header"> 
+        <nav class="nav-header">
             <ul class="ul-nav-header">
                 <li class="ul-nav-header-li"><a href="index.php">Home</a></li>
-                <li class="ul-nav-header-li"><a href="message.php">Messages</a></li>
+                <?php
+                if (isset($_SESSION['userId'])) {
+                    echo '<li class="ul-nav-header-li"><a href="message.php">Messages</a></li>';
+                }
+                ?>
                 <li class="ul-nav-header-li"><a href="./about.php">About Me</a></li>
                 <li class="ul-nav-header-li"><a href="./contacts.php">Contacts</a></li>
             </ul>
             <div class="pages-nav-header">
 
-            <?php 
+                <?php
 
                 if (isset($_SESSION['userName']) && isset($_SESSION['userId'])) {
                     echo '<div class="profile">';
@@ -32,29 +38,27 @@
 
 
                     if ($img === 1) {
-                        $fileName = 'uploads/profile'. $_SESSION['userId'] . '.' . '*';
+                        $fileName = 'uploads/profile' . $_SESSION['userId'] . '.' . '*';
                         $fileInfo = glob($fileName);
                         $fileExt = explode('.', $fileInfo[0]);
                         $fileActualExt = strtolower(end($fileExt));
 
-                        echo '<img src="uploads/profile'.$id.'.'.$fileActualExt.'?'.mt_rand().'" alt="Profile Image">';
-                    }
-                    else {
+                        echo '<img src="uploads/profile' . $id . '.' . $fileActualExt . '?' . mt_rand() . '" alt="Profile Image">';
+                    } else {
                         echo '<img src="uploads/profiledefault.jpg" alt="Profile Image">';
                     }
-                    echo '<p>'.$_SESSION['userName'].'</p>';
+                    echo '<p>' . $_SESSION['userName'] . '</p>';
                     echo '</div>';
                     echo '
                         <form action="includes/logout.inc.php" method="post" class="logout-button">
                         <button type="submit" name="logout-submit"  class="submit-button">Logout</button>
                         </form>';
-                }
-                else {
+                } else {
                     echo '<a href="login.php">Login</a>
                           <a href="signup.php">Signup</a>';
                 }
-                
-            ?>
+
+                ?>
             </div>
         </nav>
     </header>
--- loginsystem/message.php
+++ loginsystem/message.php
@@ -1,98 +1,136 @@
 <?php
-    session_start();
-    include 'includes/database.inc.php';
-    include 'header.php';
+session_start();
+include 'includes/database.inc.php';
+include 'header.php';
 ?>
 
 <main class="messages">
+
+    <?php
+
+    if (!isset($_SESSION['userId'])) {
+        echo '<h1>You are logged out</h1>';
+        exit();
+    }
+
+    ?>
 
     <div class="contact-list">
 
         <h1>Messages</h1>
         <div class="friends">
             <?php
-            
-                $user_id = $_SESSION['userId'];
 
-                $sql = "SELECT * FROM friends WHERE user_id=?"; 
-                $stmt = mysqli_stmt_init($conn);
-            
-                if (!mysqli_stmt_prepare($stmt, $sql)) {
-                    header("Location: index.php?error");
-                    exit();
-                }
-                mysqli_stmt_bind_param($stmt, "s", $user_id);
-                mysqli_stmt_execute($stmt);
-                $result = mysqli_stmt_get_result($stmt);
-                
-                if (mysqli_num_rows($result) > 0) {
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        
-                        $fid = $row['friend_id'];
-        
-                echo '  <a class="user-message" href="./message.php?friendId='.$fid.'">
+            $user_id = $_SESSION['userId'];
+
+            $sql = "SELECT * FROM friends WHERE user_id=?";
+            $stmt = mysqli_stmt_init($conn);
+
+            if (!mysqli_stmt_prepare($stmt, $sql)) {
+                header("Location: index.php?error");
+                exit();
+            }
+            mysqli_stmt_bind_param($stmt, "s", $user_id);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+
+                    $fid = $row['friend_id'];
+
+                    echo '  <a class="user-message" href="./message.php?friendId=' . $fid . '">
                             <div class="userImg-message"> ';
 
-                                $sqlImg = "SELECT * FROM profileimg WHERE user_id=?";
-                                $stmt = mysqli_stmt_init($conn);
-                                if (!mysqli_stmt_prepare($stmt, $sqlImg)) {
-                                    header("Location: index.php?sqlError");
-                                    exit();
-                                }
-                                mysqli_stmt_bind_param($stmt, "s", $fid);
-                                mysqli_stmt_execute($stmt);
-                                $resultImg = mysqli_stmt_get_result($stmt);
-                                
-                                if (mysqli_num_rows($resultImg) == 1) {
-                                    $rowImg = mysqli_fetch_assoc($resultImg);
-                                    $file = 'uploads/profile'.$fid.'.'.'*';
-                                    $fileInfo = glob($file);
-                                    $fileExt = explode('.', $fileInfo[0]);
-                                    $fileActExt = strtolower(end($fileExt));
+                    $sqlImg = "SELECT * FROM profileimg WHERE user_id=?";
+                    $stmt = mysqli_stmt_init($conn);
+                    if (!mysqli_stmt_prepare($stmt, $sqlImg)) {
+                        header("Location: index.php?sqlError");
+                        exit();
+                    }
+                    mysqli_stmt_bind_param($stmt, "s", $fid);
+                    mysqli_stmt_execute($stmt);
+                    $resultImg = mysqli_stmt_get_result($stmt);
 
-                                    if ($rowImg['status'] == 1) {
-                                        echo '<img src="uploads/profile'.$fid.'.'.$fileActExt.'?'.mt_rand().'" alt="profile_image">';
-                                    }
-                                    else {
-                                        echo '<img src="uploads/profiledefault.jpg" alt="profile_image">';
-                                    }
-                                }
+                    if (mysqli_num_rows($resultImg) == 1) {
+                        $rowImg = mysqli_fetch_assoc($resultImg);
+                        $file = 'uploads/profile' . $fid . '.' . '*';
+                        $fileInfo = glob($file);
+                        $fileExt = explode('.', $fileInfo[0]);
+                        $fileActExt = strtolower(end($fileExt));
+
+                        if ($rowImg['status'] == 1) {
+                            echo '<img src="uploads/profile' . $fid . '.' . $fileActExt . '?' . mt_rand() . '" alt="profile_image">';
+                        } else {
+                            echo '<img src="uploads/profiledefault.jpg" alt="profile_image">';
+                        }
+                    }
                     echo  '</div>';
 
-                                $sqlFriend = "SELECT * FROM users WHERE user_id=?;";
-                                $stmt = mysqli_stmt_init($conn);
+                    $sqlFriend = "SELECT * FROM users WHERE user_id=?;";
+                    $stmt = mysqli_stmt_init($conn);
 
-                                if (!mysqli_stmt_prepare($stmt, $sqlFriend)) {
-                                    echo 'Error querying database';
-                                    exit();
-                                }
-                                mysqli_stmt_bind_param($stmt, "s", $fid);
-                                mysqli_stmt_execute($stmt);
-                                $resultsFriends = mysqli_stmt_get_result($stmt);
-                                $numOfResults = mysqli_num_rows($resultsFriends);
-                                
-                                if ($numOfResults > 1) {
-                                    echo "An erro occured. Blocking process...";
-                                    exit();
-                                }
-                                $row = mysqli_fetch_assoc($resultsFriends);
+                    if (!mysqli_stmt_prepare($stmt, $sqlFriend)) {
+                        echo 'Error querying database';
+                        exit();
+                    }
+                    mysqli_stmt_bind_param($stmt, "s", $fid);
+                    mysqli_stmt_execute($stmt);
+                    $resultsFriends = mysqli_stmt_get_result($stmt);
+                    $numOfResults = mysqli_num_rows($resultsFriends);
 
-                    echo'   <div class="userInfo-message">
-                                <h4>'.$row['uname'].'</h4>
+                    if ($numOfResults > 1) {
+                        echo "An erro occured. Blocking process...";
+                        exit();
+                    }
+                    $row = mysqli_fetch_assoc($resultsFriends);
+
+                    echo '   <div class="userInfo-message">
+                                <h4>' . $row['uname'] . '</h4>
                                 <p class="last-message">Lorem ipsum dolor sit amet consectetur</p>
                             </div>
                         </a>';
-                    }  
-                }  
+                }
+            }
             ?>
         </div>
-        
+
     </div>
 
     <div class="text-area">
         <div class="user-info">
             <div class="userImg">
-                <img src="uploads/profiledefault.jpg" alt="profile" class="">
+                <?php
+
+                if (!isset($_GET['friendId'])) {
+                } else {
+                    $fid = $_GET['friendId'];
+
+                    $sqlImg = "SELECT * FROM profileimg WHERE user_id=?";
+                    $stmt = mysqli_stmt_init($conn);
+                    if (!mysqli_stmt_prepare($stmt, $sqlImg)) {
+                        header("Location: index.php?sqlError");
+                        exit();
+                    }
+                    mysqli_stmt_bind_param($stmt, "s", $fid);
+                    mysqli_stmt_execute($stmt);
+                    $resultImg = mysqli_stmt_get_result($stmt);
+
+                    if (mysqli_num_rows($resultImg) == 1) {
+                        $rowImg = mysqli_fetch_assoc($resultImg);
+                        $file = 'uploads/profile' . $fid . '.' . '*';
+                        $fileInfo = glob($file);
+                        $fileExt = explode('.', $fileInfo[0]);
+                        $fileActExt = strtolower(end($fileExt));
+
+                        if ($rowImg['status'] == 1) {
+                            echo '<img src="uploads/profile' . $fid . '.' . $fileActExt . '?' . mt_rand() . '" alt="profile_image">';
+                        } else {
+                            echo '<img src="uploads/profiledefault.jpg" alt="profile_image">';
+                        }
+                    }
+                }
+                ?>
             </div>
             <p class="uname">Username</p>
         </div>
@@ -124,10 +162,10 @@
         </div>
         <div class="send-area">
             <!-- <div class="message-form"> -->
-                <form action="">
-                    <textarea name="message" id="message"></textarea>
-                    <button type="submit" name="send-message" class="send-message"><i class="fas fa-paper-plane"></i></button>
-                </form>
+            <form action="">
+                <textarea name="message" id="message"></textarea>
+                <button type="submit" name="send-message" class="send-message"><i class="fas fa-paper-plane"></i></button>
+            </form>
             <!-- </div> -->
         </div>
     </div>
